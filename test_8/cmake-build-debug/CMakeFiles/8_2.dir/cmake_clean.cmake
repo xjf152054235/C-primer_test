ae8441bfbb474530929885187142b3a92b61e7a6
xjf <793414725@.qq.com>
2021-12-28 10:18:08 +0800

    IO类

---
+++ test_8/cmake-build-debug/CMakeFiles/8_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "8_2"
+  "8_2.pdb"
+  "CMakeFiles/8_2.dir/8_2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
