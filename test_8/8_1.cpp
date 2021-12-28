425c205cfcf3ce4fa148cf4d6541112610d372f2
xjf <793414725@.qq.com>
2021-12-28 10:01:00 +0800

    IO类

--- test_8/8_1.cpp
+++ test_8/8_1.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
+//编写函数，接受一个istream&参数，返回值类型也是istream&。
+// 此函数须从给定流中读取数据，直至遇到文件结束标识时停止。
+// 它将读取的数据打印在标准输出上。完成这些操作后，在返回流之前，对流进行复位，使其处于有效状态。
+std::istream& func(std::istream &is)
+{
+    std::string buf;
+    while(is>>buf)
+        std::cout<<buf<<std::endl;
+    is.clear();
+    return is;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
